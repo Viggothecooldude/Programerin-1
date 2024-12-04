b6957799fa80800bbe045c4a50c879c568f868f1
0707VILI <0707vili@gapp.uddevalla.se>
2024-12-04 14:28:32 +0100

    y

--- yatsy/ConsoleApp1/Program.cs
+++ yatsy/ConsoleApp1/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System.Net.Http.Headers;
+using System.Runtime.InteropServices.Marshalling;
+Random tarningslag = new Random();
+string keep;
+int[] tarning = new int[5];
 
 int avsluta=0;
-Random tarningslag = new Random();
-int[] tarning = new int[5];
-while ( avsluta ==0 ) {
+while (avsluta == 0) {
     for (int i = 0; i < 5; i++)
     {
-       
+
         tarning[i] = tarningslag.Next(1, 7);
     }
     
+    //asci-art
     string[] tarningsbilder = [" -----\r\n|     |\r\n|  o  |\r\n|     |\r\n -----\r", "\r -----\r\n|  o  |\r\n|     |\r\n|  o  |\r\n -----\r", "\r -----\r\n| o   |\r\n|  o  |\r\n|   o |\r\n -----\r", "\r -----\r\n| o o |\r\n|     |\r\n| o o |\r\n -----\r", "\r -----\r\n| o o |\r\n|  o  |\r\n| o o |\r\n -----\r", "\r -----\r\n| o o |\r\n| o o |\r\n| o o |\r\n -----"];
 
     int antalpar = 0;
@@ -17,27 +20,42 @@ while ( avsluta ==0 ) {
     bool fyrtal = false;
     bool yatzy = false;
     int poang = 0;
-    
-    bool[] keep = new bool[5];
-
+    int[] slag = new int[5];
 
     Console.WriteLine("tryck enter för att slå tärningar");
     Console.ReadLine();
-    for (int i = 0; i < 5; i++)
+    for (int e = 0; e < 3; e++)
     {
-        Console.WriteLine(i);
-        Console.WriteLine(tarning[i]);
-        Console.WriteLine(tarningsbilder[tarning[i]-1]);
-    }
-    
+        //koll på omslag
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(tarningsbilder[tarning[i] - 1]);
         }
-    
+        if (e < 2)
+        {
+            Console.WriteLine("Vilka tärningar vill du slå om? (1 för att spara 0 för att slå om t.ex 10010)");
+            keep = Console.ReadLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                //visa tärningar
+                if (keep[i] == '0')
+                {
+                    tarning[i] = tarningslag.Next(1, 7);
+                }
+
+            }
+        }
+    }
+
+    //koll på hur många av samma nummer det finns för att kolla par,triss m.m
     for (int i = 1; i <= 6; i++)
     {
         int portal = 0;
         for (int j = 0; j < 5; j++)
         {
-            if (i == slag[j])
+            if (i == tarning[j])
             { portal++; }
         }
         if (portal==2)
@@ -60,10 +78,46 @@ while ( avsluta ==0 ) {
             yatzy=true;
             poang += 50;
         }
+        Console.WriteLine(portal);
     }
-    Console.WriteLine("Resultat");
-    Console.WriteLine(antalpar);
-    Console.WriteLine(triss);
-    Console.WriteLine(fyrtal);
-    Console.WriteLine(yatzy);
+
+    //poängutdelning
+    if (yatzy == true)
+    {
+        Console.WriteLine("Du fick yatsy + 50 poäng");
+    }
+    else if (fyrtal == true)
+    {
+        Console.WriteLine("Du fick fyrtal + " + poang + " poäng");
+    }
+    else if (triss == true && antalpar == 1)
+    {
+        Console.WriteLine("Du fick kåk + " + poang + " poäng");
+    }
+    else if (triss == true)
+    {
+        Console.WriteLine("Du fick triss + " + poang + " poäng");
+    }
+    else if (antalpar == 2)
+    {
+        Console.WriteLine("Du fick tvåpar + " + poang + " poäng");
+    }
+    else if (antalpar == 1)
+    {
+        Console.WriteLine("Du fick par + " + poang + " poäng");
+    }
+    else if (!slag.Contains(6))
+    {
+        poang += 15;
+        Console.WriteLine("Du fick lilla stegen + " + poang + " poäng");
+    }
+    else if (!slag.Contains(1))
+    {
+        poang += 20;
+        Console.WriteLine("Du fick stora stegen + " + poang + " poäng");
+    }
+    else
+    {
+        Console.WriteLine("du fick inget 0 poäng");
+    }
 }
